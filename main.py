8f23061f7d03a22517dbb8e157710e85a5cfdaaa
charles <charlesiddle99@gmail.com>
2022-03-24 11:05:12 +0300

    two_sum_v2

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 class Solution(object):
 
+ class Solution(object):
     def find_num(self, num, target):
         num_to_find = target - num
         return num_to_find
+    
+    def find_two_sum(self, num_dict, num_to_find, idx):
+        two_sum_indexes = [num_dict[num_to_find], idx]
+        return two_sum_indexes
 
     def twoSum(self, nums, target):
         num_dict = {}
         two_sum_indexes = []
         for idx, num in enumerate(nums):
-            # num_dict[num] = idx
             num_to_find = self.find_num(num, target)
-            print(num_dict)
             if num_to_find in num_dict:
-                two_sum_indexes = [num_dict[num_to_find], idx]
-                # return two_sum_indexes
+                two_sum_indexes = self.find_two_sum(num_dict, num_to_find, idx)
+                return two_sum_indexes
             else:
                 num_dict[num] = idx
-
-        if len(two_sum_indexes) > 0:
-            print(two_sum_indexes)
-        else:
-            print("We could not find any two such numbers")
 
 
 
